b0d28dbedb5adf9b43d6e118ed32b4caff2fad3a
Chen Zheng <czheng1968@gmail.com>
2015-05-11 10:53:55 -0400

    removed scrap code

--- src/TimeZones/TimeZones.php
+++ src/TimeZones/TimeZones.php
@@ -16,18 +16,9 @@ class TimeZones extends PluginBase implements Listener {
 	
     if(!is_dir($this->getDataFolder())) {
       mkdir($this->getDataFolder());
+      $this->saveDefaultConfig();
     }
-    #makes the config (important)
-    if(!file_exists($this->getDataFolder() . "config.yml")) {
-      $this->getLogger()->info("Config.yml file detected!");
-    }else{//if file don't exist, this create it and put "corrector" value
-      $this->config = new Config( $this->getDataFolder() . "config.yml", Config::YAML);
-      $this->getLogger()->info("Creating files...");
-      $this->config->set("corrector:", 0);
-      $this->config->set("meridiem:", "AM");
-      $this->config->save();
-      $this->getLogger()->info("Files created!");
-    }
+    
       
       
         $this->getLogger()->info("TimeZones plugin enabled!");
@@ -35,37 +26,13 @@ class TimeZones extends PluginBase implements Listener {
         $this->getLogger()->info("Current time is:");
         
         $commanderPackage = $this->getConfig()->get("corrector");
-        $merid = $this->getConfig()->get("meridiem");
         
-        if ($merid === "AM"){
-           $mmrd = "AM";
-        }
-        elseif ($merid === "PM"){
-           $mmrd = "PM";
-        }
-        else{
-           $this->getLogger()->warning("The AM/PM is not correct. Set to AM/PM.");
-        }
         $correctHour = date('g');
         $dater = $correctHour + $commanderPackage;
         $timers = date('i:s');
-        $this->getLogger()->info($dater . ":" . $timers . $mmrd);
+        $this->getLogger()->info($dater . ":" . $timers );
         
-        #AM PM hour change (not tested)
-        for (;;) {
-         if ($correctHour === "12"){
-             if ($mmrd === "AM"){
-             	$mmrd = "PM";
-             }
-             
-             elseif ($mmrd === "PM"){
-             	$mmrd = "AM";
-             }
-             
-         }
-         
-    }
-    }  
+        
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
     if(strtolower($command->getName()) === "tz"){
         
